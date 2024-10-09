4f0a6a3c7d98e0bb31f833864e73624e379d1882
wishabc <aswq22013@gmail.com>
2024-10-08 21:39:53 -0700

    Refactor logging format in main.py

--- hotspot2/main.py
+++ hotspot2/main.py
@@ -20,7 +20,7 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
-logger = mp.get_logger()
+logger = logging.getLogger(__name__)
 
 
 @dataclasses.dataclass
