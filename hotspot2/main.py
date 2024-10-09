1586b0423edcc60e68eb216444fcbee1688977f8
wishabc <aswq22013@gmail.com>
2024-10-08 21:28:44 -0700

    Refactor GenomeProcessor initialization to use int32 data type for signal_tr threshold

--- hotspot2/main.py
+++ hotspot2/main.py
@@ -38,7 +38,7 @@ class GenomeProcessor:
     """
     Base class to run hotspot2 on a whole genome
     """
-    def __init__(self, chrom_sizes, mappable_bases_file=None, window=201, bg_window=50001, min_mappable_bg=10000, signal_tr=0.975, int_dtype = np.uint32, fdr_method='fdr_bh', cpus=1) -> None:
+    def __init__(self, chrom_sizes, mappable_bases_file=None, window=201, bg_window=50001, min_mappable_bg=10000, signal_tr=0.975, int_dtype = np.int32, fdr_method='fdr_bh', cpus=1) -> None:
         self.chrom_sizes = chrom_sizes
         self.mappable_bases_file = mappable_bases_file
         self.min_mappable_bg = min_mappable_bg
