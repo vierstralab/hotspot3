0ad21dc991f395beae5bcf49207a884d3786325a
wishabc <aswq22013@gmail.com>
2024-10-25 15:26:31 -0700

    refactor: Update running_nansum method in WindowBackgroundFit to handle missing values

--- src/hotspot3/fit.py
+++ src/hotspot3/fit.py
@@ -88,5 +88,5 @@ class WindowBackgroundFit(BackgroundFit):
     @wrap_masked
     def running_nansum(self, array, window):
         array = array.astype(np.float32)
-        return bn.move_sum(array, window, min_count=1).astype(np.float32)
+        return bn.move_sum(array, window, min_count=self.config.min_mappable_bg).astype(np.float32)
     
