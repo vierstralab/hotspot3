9ea79af663b74ec8db2f9c3586dd4475514717ff
wishabc <aswq22013@gmail.com>
2024-11-03 12:36:03 -0800

    Refactor code to update GenomeSegmentator states in ChromosomeProcessor

--- src/hotspot3/processors.py
+++ src/hotspot3/processors.py
@@ -362,6 +362,7 @@ class ChromosomeProcessor:
         snps_collection = GenomeSNPsHandler(chrom_data_df, chromosomes_wrapper)
         bad = 1-global_p / (global_p)
         bad = bad if bad > 1 else 1/bad
+        print(bad)
         gs = GenomeSegmentator(
             snps_collection=snps_collection.data,
             out=f"{self.chrom_name}.test.bed",
@@ -370,7 +371,7 @@ class ChromosomeProcessor:
             logger_level=self.config.logger_level,
             segmentation_mode='binomial',
             chromosomes_wrapper=chromosomes_wrapper,
-            states=np.arange(1, 6) * bad
+            states=[1, *np.arange(1, 6) * bad]
         )
         gs.estimate_BAD()
         self.gp.logger.debug(f"{self.chrom_name}: Signal quantile: {global_fit.fit_quantile:.3f}. signal threshold: {global_fit.fit_threshold:.0f}. Best RMSEA: {global_fit.rmsea:.3f}")
