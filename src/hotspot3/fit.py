f94df9ca08b0d9c48e5b9713f88e3732121f6088
wishabc <aswq22013@gmail.com>
2024-11-02 20:17:14 -0700

    Refactor code to change dtype from np.float16 to np.float32 in fit.py

--- src/hotspot3/fit.py
+++ src/hotspot3/fit.py
@@ -315,10 +315,10 @@ class StridedFit(BackgroundFit):
         best_tr_with_nan = np.full_like(array, np.nan, dtype=np.float32)
         best_tr_with_nan[subsampled_indices] = best_tr
 
-        best_rmsea_with_nan = np.full_like(array, np.nan, dtype=np.float16)
+        best_rmsea_with_nan = np.full_like(array, np.nan, dtype=np.float32)
         best_rmsea_with_nan[subsampled_indices] = best_rmsea
 
-        best_quantile_with_nan = np.full_like(array, np.nan, dtype=np.float16)
+        best_quantile_with_nan = np.full_like(array, np.nan, dtype=np.float32)
         best_quantile_with_nan[subsampled_indices] = best_quantile
 
         return best_tr_with_nan, best_quantile_with_nan, best_rmsea_with_nan
