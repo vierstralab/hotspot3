8758f173774a6a6d2edfce2369a322faaa3e51cd
wishabc <aswq22013@gmail.com>
2024-11-02 21:18:02 -0700

    Refactor code to reverse the order of iteration in GlobalBackgroundFit

--- src/hotspot3/fit.py
+++ src/hotspot3/fit.py
@@ -100,12 +100,12 @@ class GlobalBackgroundFit(BackgroundFit):
 
         res = []
         trs, _ = self.get_signal_bins(agg_cutcounts)
-        for tr in trs[::-1]:
+        for tr in trs:
             p, r = self.fit_for_tr(agg_cutcounts, tr)
             rmsea = self.calc_rmsea_for_tr(counts, unique, p, r, tr)
             res.append((tr, rmsea))
-            if rmsea <= self.config.rmsea_tr:
-                break
+            # if rmsea <= self.config.rmsea_tr:
+            #     break
         else:
             tr, rmsea = min(res, key=lambda x: x[1])
         quantile = np.sum(agg_cutcounts < tr) / agg_cutcounts.shape[0]
