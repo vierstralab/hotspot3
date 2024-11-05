db5230b096c77c9aebca5509857beb6c17a72366
wishabc <aswq22013@gmail.com>
2024-11-05 14:29:50 -0800

    debug

--- src/hotspot3/babachi.py
+++ src/hotspot3/babachi.py
@@ -45,9 +45,9 @@ class Segmentation(WithLogger):
             logger=self.logger,
             allele_reads_tr=0,
             b_penalty=5,
-            min_seg_bp=5000,
-            min_seg_snps=0,
-            subchr_filter=0
+            # min_seg_bp=5000,
+            # min_seg_snps=0,
+            # subchr_filter=0
         )
         bad_segments = gs.estimate_BAD()
         gs.write_BAD(bad_segments, f"{chrom_name}.test.bed")
