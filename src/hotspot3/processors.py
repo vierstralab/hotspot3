6a332c7223a3e7a075bc1cd7e643d54bd19cfd91
wishabc <aswq22013@gmail.com>
2024-11-12 10:41:26 -0800

    Refactor GenomeProcessor to update error handling and add parallel function error handler

--- src/hotspot3/processors.py
+++ src/hotspot3/processors.py
@@ -3,6 +3,7 @@ from concurrent.futures import ProcessPoolExecutor
 import multiprocessing as mp
 import pandas as pd
 from typing import Iterable, List, cast, Callable, Any
+import functools
 
 from genome_tools.genomic_interval import GenomicInterval
 
@@ -102,7 +103,16 @@ class GenomeProcessor(WithLogger):
                 reformat_arg = [arg] * len(self.chromosome_processors)
             res_args.append(reformat_arg)
         return [self.chromosome_processors, *res_args]
-    
+
+    def parallel_func_error_handler(self, func: Callable[..., Any]):
+        @functools.wraps(func)
+        def wrapper(processor: 'ChromosomeProcessor', *args):
+            try:
+                return func(processor, *args)
+            except:
+                self.logger.exception(f"Exception occured in {func.__name__} for chromosome {processor.chrom_name}")
+                raise
+        return wrapper
 
     def parallel_by_chromosome(self, func, *args, cpus=None):
         """
@@ -112,7 +122,7 @@ class GenomeProcessor(WithLogger):
             cpus = self.cpus
         args = self.construct_parallel_args(*args)
         self.logger.debug(f'Using {cpus} CPUs to {func.__name__}')
-        func = parallel_func_error_handler(func)
+        func = self.parallel_func_error_handler(func)
         results = []
         if self.cpus == 1:
             for func_args in zip(*args):
@@ -605,13 +615,3 @@ class ChromosomeProcessor(WithLoggerAndInterval):
             chrom_names=[x for x in self.gp.chrom_sizes.keys()],
             compression_level=compression_level,
         )
-
-
-def parallel_func_error_handler(func):
-    def wrapper(processor: 'ChromosomeProcessor', *args):
-        try:
-            return func(processor, *args)
-        except:
-            processor.logger.critical(f"Exception occured in {func.__name__} for chromosome {processor.chrom_name}")
-            raise
-    return wrapper
