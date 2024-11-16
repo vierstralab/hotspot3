Refactor fit.py to fix background sum calculation bug and add optional 'where' parameter in BackgroundFit class
Refactor segment_fit.py to use global fit threshold in ChromosomeFit class

Refactor main.py to rename quantile_tr_bb_path to background_bb_path and update method call in genome_processor

Refactor processors.py to change ChromosomeFit to GlobalFit and remove unnecessary code

src/hotspot3/background_fit/fit.py
@@ -143,7 +143,7 @@ class BackgroundFit(BottleneckWrapper):
         """
         valid_bins = np.ones_like(value_counts_per_bin, dtype=bool)
         bg_sum_mappable = np.sum(value_counts_per_bin, axis=0, where=valid_bins)
-        
+
         sf_values = np.where(bin_edges == 0, 1., betainc(bin_edges, r, p))
         sf_diffs = -np.diff(sf_values, axis=0)
         assert sf_diffs.shape == value_counts_per_bin.shape, f"SF diffs shape should match value counts shape. Got SF: {sf_diffs.shape} and vc: {value_counts_per_bin.shape}"
@@ -311,6 +311,7 @@ class GlobalBackgroundFit(BackgroundFit):
         return p, r, rmsea
 
 
+## Currently deprecated ##
 class StridedBackgroundFit(BackgroundFit):
     """
     Class to fit the background distribution using a strided approach.

src/hotspot3/background_fit/segment_fit.py
@@ -138,12 +138,10 @@ class ChromosomeFit(WithLoggerAndInterval):
                 fallback_fit_results=fallback_fit_results,
                 step=step
             )
-
-        strided_fit = self.copy_with_params(StridedBackgroundFit)
-        fit_results = strided_fit.fit(
+        fit_threshold = np.full_like(
             signal_at_segment,
-            fallback_fit_results=global_fit_results
+            global_fit_results.fit_threshold,
+            dtype=np.float32
         )
-        fit_results.fit_threshold = interpolate_nan(fit_results.fit_threshold)
         self.logger.debug(f"{self.name}: Signal thresholds approximated")
-        return fit_results.fit_threshold, fit_results.rmsea, global_fit_results
+        return fit_threshold, global_fit_results

src/hotspot3/processors.py
@@ -489,8 +489,8 @@ class ChromosomeProcessor(WithLoggerAndInterval):
         )
         self.logger.debug(f'{self.chrom_name}: Estimating proportion of background vs signal')
         
-        s_fit = self.copy_with_params(ChromosomeFit)
-        per_window_trs_global, rmseas, global_fit_params = s_fit.fit_segment_thresholds(
+        s_fit = self.copy_with_params(ChromosomeFit) # Change to GlobalFit
+        per_window_trs_global, global_fit_params = s_fit.fit_segment_thresholds(
             agg_cutcounts,
         )
         
@@ -498,13 +498,6 @@ class ChromosomeProcessor(WithLoggerAndInterval):
             self.logger.warning(f"{self.chrom_name}: Best RMSEA: {global_fit_params.rmsea:.3f}. Chromosome fit might be poorly approximated.")
 
         self.logger.debug(f"{self.chrom_name}: Signal quantile: {global_fit_params.fit_quantile:.3f}. signal threshold: {global_fit_params.fit_threshold:.0f}. Best RMSEA: {global_fit_params.rmsea:.3f}")
-
-        good_fits_n = np.sum(rmseas <= self.config.rmsea_tr)
-        n_rmsea = np.sum(~np.isnan(rmseas))
-        self.logger.debug(f"{self.chrom_name}: Signal thresholds approximated. {good_fits_n:,}/{n_rmsea:,} strided windows have RMSEA <= {self.config.rmsea_tr:.2f}")
-
-        # HOTFIX
-        per_window_trs_global = np.full_like(per_window_trs_global, global_fit_params.fit_threshold)
 
         # Segmentation
         segmentation = self.copy_with_params(BabachiWrapper)