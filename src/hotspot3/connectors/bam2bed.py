37b2677992b972d470fa2153411f7e075aa4da84
wishabc <aswq22013@gmail.com>
2024-12-11 19:06:52 +0700

    Add print statement to display result in run_bam2bed function

--- src/hotspot3/connectors/bam2bed.py
+++ src/hotspot3/connectors/bam2bed.py
@@ -19,6 +19,7 @@ def run_bam2bed(*args, reference_fasta=None):
             capture_output=True,
             shell=True,
         )
+        print(result)
     return result
 
 def fasta_as_arg(reference_fasta):
