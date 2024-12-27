af729cc1cf43b8083688dc473cb93dca3b8535d2
wishabc <aswq22013@gmail.com>
2024-12-27 19:39:06 +0700

    Add debug print statement for bad segments count in ChromosomeProcessor

--- src/hotspot3/processors.py
+++ src/hotspot3/processors.py
@@ -559,8 +559,7 @@ class ChromosomeProcessor(WithLoggerAndInterval):
             per_window_trs_global,
             global_fit_params
         )
-        if len(bad_segments) == 0:
-            raise NotEnoughDataForContig
+        print(len(bad_segments))
         segments_fit = self.copy_with_params(SegmentalFit)
         per_interval_params = segments_fit.fit_per_segment_bg_model(
             agg_cutcounts=agg_cutcounts,
