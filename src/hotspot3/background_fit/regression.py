c87d520317c7a408fdafa184bc1ef5da43fee87b
wishabc <aswq22013@gmail.com>
2024-12-31 12:59:23 +0700

    Increase precision in SignalToNoiseFit calculation to address rounding issues

--- src/hotspot3/background_fit/regression.py
+++ src/hotspot3/background_fit/regression.py
@@ -69,7 +69,7 @@ class SignalToNoiseFit(WithLogger):
             expit(
                 -y_pred - np.log(self.config.outlier_segment_threshold)
             ), 
-            2
+            3
         ) # Ceil to 3th decimal place to avoid precision issues
         outlier_dist = np.exp(resid)
 
