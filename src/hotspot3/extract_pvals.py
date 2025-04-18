42c83f4d9a5a52aa124608aad55907a0e9e68675
wishabc <aswq22013@gmail.com>
2025-04-18 09:07:44 +0700

    Add extract_max_pval function to streamline maximum p-value extraction from chromosome data

--- src/hotspot3/extract_pvals.py
+++ src/hotspot3/extract_pvals.py
@@ -29,6 +29,8 @@ def parse_args():
 
     return parser.parse_args()
 
+def extract_max_pval(row, chrom_pvals):
+    return np.nanmax(chrom_pvals[row['start']:row['end']], initial=0)
 
 def main():
     args = parse_args()
@@ -45,15 +47,14 @@ def main():
         if chrom not in chrom_sizes:
             continue
         chrom_interval = GenomicInterval(chrom, 0, chrom_sizes[chrom])
-        max_neglog_p = np.zeros(len(group), dtype=np.float32)
 
         chrom_reader = ChromReader(genomic_interval=chrom_interval)
         chrom_pvals = chrom_reader.extract_from_parquet(
             args.pvals_parquet,
             columns=['log10_pval']
-        )['log10_pval'].values.astype(np.float32)
-        segment_max(chrom_pvals, group['start'].values, group['end'].values, max_neglog_p)
-        group['max_neglog_p'] = max_neglog_p
+        )['log10_pval'].values
+        
+        group['max_neglog_p'] = group.apply(extract_max_pval, chrom_pvals=chrom_pvals, axis=1)
         data.append(group)
 
     data = pd.concat(data, ignore_index=True)
