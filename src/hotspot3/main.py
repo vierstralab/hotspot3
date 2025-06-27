62d9382d83a1d050b0dd8800ec12fba42c97939d
wishabc <aswq22013@gmail.com>
2025-06-27 22:29:22 +0700

    Add print statement to display result in find_missing_steps function

--- src/hotspot3/main.py
+++ src/hotspot3/main.py
@@ -56,6 +56,7 @@ def find_missing_steps(paths: Hotspot3Paths, save_density):
     if save_density:
         outputs.add('normalized_density')
     result = resolve_required_steps(outputs, inputs, step_graph)
+    print(result)
     if 'bam' in result:
         raise ValueError("Provide a bam file or cutcounts")
     return result
